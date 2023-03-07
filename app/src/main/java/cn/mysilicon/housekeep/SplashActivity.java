13584aab5bd1c3097af261351eabb17906811c2f
mysilicon <mysilicon@163.com>
2023-03-07 10:24:56 +0800

    启动界面

---
+++ app/src/main/java/cn/mysilicon/housekeep/SplashActivity.java
@@ -0,0 +1,36 @@
+package cn.mysilicon.housekeep;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //隐藏状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //隐藏标题栏
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        //创建子线程
+        Thread mThread=new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(2000);//使程序休眠3秒
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        mThread.start();//启动线程
+    }
+}
